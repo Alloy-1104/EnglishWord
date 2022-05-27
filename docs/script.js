6de2ba37cde9786f10738f8b96244d16cf5f0ef9
Alloy-1104 <crafter.alloy@gmail.com>
2022-05-27 20:42:48 +0900

    5

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,16 @@
-//const jsonData= require('./words.json'); 
-//console.log(jsonData[0][0]);
+function viewChange() {
+  if (document.getElementById("mode-list")) {
+    var id = document.getElementById("mode-list").value;
+    var elements = Array.from(
+      document.getElementsByClassName("setting_content")
+    );
+    elements.forEach(function (element) {
+      if(element.id == id + "-content") {
+        element.style.display = "block";
+      } else if(element.id != id + "-content") {
+        element.style.display = "none";
+      }
+    });
+  }
+  window.onload = viewChange;
+}
